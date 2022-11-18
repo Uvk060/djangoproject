15ca2c7cbf2ba9fb5fed890f527ac16b1a1fcb5c
Uvk060 <uvk060@gmail.com>
2022-11-17 20:45:23 -0800

    content changes

--- newyear/views.py
+++ newyear/views.py
@@ -6,5 +6,5 @@ def index(request):
     now = datetime.datetime.now()
     return render(request,'newyear/index.html', {
         "newyear": now.month ==1 and now.day == 1,
-        "title": 'Good evening my sleepy user' if now.hour >= 18 or now.hour <= 6  else 'Good day my happy user'
+        "title": 'Good evening, my sleepy user' if now.hour >= 20 or now.hour <= 6  else 'Good day, my happy user'
         })
