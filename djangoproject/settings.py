58da5ccf8b881c1d3c08f34b5196ee4c8b579bee
Uvk060 <uvk060@gmail.com>
2022-11-18 16:51:14 -0800

    app tasks and add new task with simple css

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hello',
-    "newyear",
+    'newyear',
+    'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
